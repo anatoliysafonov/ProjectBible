bda881b4dc1bb81ad785699663bed2db9a80fc50
Анатолій <anatoliy.safonov.ua@gmail.com>
2023-06-01 13:45:13 +0300

    Update models.py

--- mysite/bible/models.py
+++ mysite/bible/models.py
@@ -21,9 +21,8 @@ class Verse(models.Model):
     text = TextField()
     chapter = IntegerField(null=False, default=1)
     verse = IntegerField(null=False, default=1)
-    # testament = ForeignKey(Testament, on_delete=models.CASCADE)
     testament = IntegerField()
-    # book = ForeignKey(Book, on_delete=models.CASCADE)
     book = IntegerField()
+
     class Meta:
         ordering = ['verse']
